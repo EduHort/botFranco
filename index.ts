743f94a410cf4d1b741cd3f0b13f987d5633bfda
EduHort <86855087+EduHort@users.noreply.github.com>
2025-03-28 11:12:11 -0300

    melhorias bot

--- index.ts
+++ index.ts
@@ -18,10 +18,10 @@ const optionsMap: { [key: string]: string } = {
     '10': 'Cancelar'
 };
 
-client.on('message', async (message) => {
+client.on('message_create', async (message) => {
     try {
         // Usar .includes para evitar espaços na mensagem
-        if (message.fromMe && message.body.includes('')) {
+        if (message.fromMe && message.body.includes('!!!!! Colocar a mensagem aqui !!!!!!')) {
             // Checa para mensagens em massa. Não fazer nada nesse caso.
         }
         else {
@@ -38,46 +38,48 @@ client.on('message', async (message) => {
                 // Registra o novo usuário no banco de dados
                 registerUserInteraction(message.to);
             }
-            // Verifica se o usuário existe e está esperando para escolher uma opção (1 a 9)
-            else if (userData && !userData.option && !userData.replyTime && !userData.rowNumber) {
-                // Verifica se o usuário escolheu uma opção válida (1-9)
-                if (/^[1-9]$/.test(message.body.trim())) {
-                    const option = optionsMap[message.body.trim()];     // Obtém a opção escolhida
+            // Verifica se o usuário existe
+            else if (userData) {
+                // Verifica se o usuário está esperando para escolher uma opção (1 a 10)
+                if (!userData.option && !message.fromMe) {
+                    // Verifica se o usuário escolheu uma opção de setor (1-8)
+                    if (/^[1-8]$/.test(message.body.trim())) {
+                        const option = optionsMap[message.body.trim()];     // Obtém a opção escolhida
 
-                    const currentTime = new Date();
-                    currentTime.setHours(currentTime.getHours() - 3);   // Ajusta o fuso horário para o Brasil
-                    const weekday = currentTime.toLocaleDateString('pt-BR', { weekday: 'short' }).toUpperCase().slice(0, 3);    // Obtém o dia da semana
+                        const currentTime = new Date();
+                        currentTime.setHours(currentTime.getHours() - 3);   // Ajusta o fuso horário para o Brasil
+                        const weekday = currentTime.toLocaleDateString('pt-BR', { weekday: 'short' }).toUpperCase().slice(0, 3);    // Obtém o dia da semana
 
-                    // Adiciona os dados do cliente na planilha Excel e retorna o número da linha
-                    const rowNumber = addRowToExcel([message.from.replace('@c.us', ''), option, currentTime.toLocaleString('pt-BR'), weekday]);
+                        // Adiciona os dados do cliente na planilha Excel e retorna o número da linha
+                        const rowNumber = addRowToExcel([userData.user.replace('@c.us', ''), option, currentTime.toLocaleString('pt-BR'), weekday]);
 
-                    if (rowNumber) {
-                        // Atualiza os dados do usuário no banco de dados com a opção, tempo e número da linha
-                        updateMessageTracking(message.from, option, currentTime.toISOString(), rowNumber);
+                        if (rowNumber) {
+                            // Atualiza os dados do usuário no banco de dados com a opção, tempo e número da linha
+                            updateMessageTracking(userData.user, option, currentTime.toISOString(), rowNumber);
+                        }
+                    }
+                    // Verifica se o cliente digitou '9' ou '10' para excluir o cadastro
+                    else if (/^(9|10)$/.test(message.body.trim())) {
+                        clearUserTrackingData(userData.user);
                     }
                 }
-                // Verifica se o cliente digitou '10' (Reiniciar o fluxo?)
-                else if (/^10$/.test(message.body.trim())) {
-                    // Limpa os dados de rastreamento do usuário no banco de dados
-                    clearUserTrackingData(message.from); 
-                }
-            }
-            // Verifica se a mensagem é do atendente e se o usuário está sendo atendido
-            else if (message.fromMe && userData && userData.option && userData.replyTime && userData.rowNumber) {
-                // Verifica se a mensagem do atendente NÃO contém frases específicas
-                if (!message.body.includes('estou encaminhando para atendimento') || !message.body.includes('Estamos fechados no momento') || !message.body.includes('Nossos atendentes estão em horário de almoço')) {
-                    const replyTime = new Date(userData.replyTime);
-                    const atendenteReplyTime = new Date();
-                    atendenteReplyTime.setHours(atendenteReplyTime.getHours() - 3);   // Ajusta o fuso horário para o Brasil
+                // Verifica se a mensagem é do atendente e se o usuário está sendo atendido
+                else if (message.fromMe && userData.option && userData.replyTime && userData.rowNumber) {
+                    // Verifica se a mensagem do atendente NÃO contém frases específicas
+                    if (!message.body.includes('estou encaminhando para atendimento') && !message.body.includes('Estamos fechados no momento') && !message.body.includes('Nossos atendentes estão em horário de almoço')) {
+                        const replyTime = new Date(userData.replyTime);
+                        const atendenteReplyTime = new Date();
+                        atendenteReplyTime.setHours(atendenteReplyTime.getHours() - 3);   // Ajusta o fuso horário para o Brasil
 
-                    // Calcula o tempo de resposta do atendente
-                    const timeDiff = calculateWorkingTime(replyTime, atendenteReplyTime);
+                        // Calcula o tempo de resposta do atendente
+                        const timeDiff = calculateWorkingTime(replyTime, atendenteReplyTime);
 
-                    // Atualiza o tempo de resposta do atendente na planilha Excel
-                    addRowToExcel([null, null, null, null, atendenteReplyTime.toLocaleString('pt-BR'), timeDiff.toFixed(2)], true, userData.rowNumber);
+                        // Atualiza o tempo de resposta do atendente na planilha Excel
+                        addRowToExcel([null, null, null, null, atendenteReplyTime.toLocaleString('pt-BR'), timeDiff.toFixed(2)], true, userData.rowNumber);
 
-                    // Limpa os dados de rastreamento do usuário no banco de dados
-                    clearUserTrackingData(message.to);
+                        // Limpa os dados de rastreamento do usuário no banco de dados
+                        clearUserTrackingData(message.to);
+                    }
                 }
             }
         }
